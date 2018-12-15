7eea5c2d0ef6cdda5253bc2862fe95858376952e
morfeush22 <patro.kacper@gmail.com>
2018-12-15 18:52:19 +0100

    tests structure

---
+++ tests/base.h
@@ -0,0 +1,16 @@
+//
+// Created by morfeush22 on 15.12.18.
+//
+
+#ifndef PROJEKT_BASE_H
+#define PROJEKT_BASE_H
+
+#define ASSERT_EQUAL(x, y)              \
+{                                       \
+    if ( (x) != (y) )                   \
+    {                                   \
+        exit(1);                        \
+    }                                   \
+}
+
+#endif //PROJEKT_BASE_H
---
+++ tests/basic_tests.c
@@ -0,0 +1,13 @@
+//
+// Created by morfeush22 on 15.12.18.
+//
+
+#include "base.h"
+
+void basicTest() {
+    ASSERT_EQUAL(1, 1);
+}
+
+int main(int argc, char** argv) {
+    basicTest();
+}
