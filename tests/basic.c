fa3dbab5ade5d939fb61a6a49f640c5726e6130c
morfeush22 <patro.kacper@gmail.com>
2019-01-05 13:18:51 +0100

    fix uts

--- tests/basic.c
+++ tests/basic.c
@@ -2,27 +2,61 @@
 // Created by morfeush22 on 15.12.18.
 //
 
+#include "mpi.h"
 #include "test_base.h"
 #include "../src/base.h"
 #include "../src/chebyshev.h"
 
 void basicTest() {
-    struct Data data = loadDataFromFile("sources/data");
-    int dimension = data.dimension;
+    int rank, size;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    FILE *fp;
+    int dimension;
+
+    if (rank == 0) {
+        fp = fopen("sources/data", "r");
+        if (fp == NULL) {
+            exit(EXIT_FAILURE);
+        }
+
+        dimension = numberOfLines(fp);
+    }
+
+    MPI_Bcast(&dimension, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    struct Data data = allocateData(dimension);
+    data.dimension = dimension;
+
+    if (rank == 0) {
+        parseData(fp, &data);
+        fclose(fp);
+    }
 
     int sParameter = 10;
     int iterations = 0;
 
-    double * result = solveLinear(data, PRECISION, sParameter, &iterations);
-    double * expectedResult = loadVectorFromFile("sources/data.expected_solution");
+    double * result;
 
-    for (int i = 0; i < dimension; ++i) {
-        ASSERT_EQUAL(expectedResult[i], result[i], PRECISION);
+    result = solveLinear(data, PRECISION, sParameter, &iterations, rank, size);
+
+    if (rank == 0) {
+        double * expectedResult = loadVectorFromFile("sources/data.expected_solution");
+
+        for (int i = 0; i < dimension; ++i) {
+            ASSERT_EQUAL(expectedResult[i], result[i], PRECISION);
+        }
+
+        free(expectedResult);
     }
 
-    deallocateData(data);
+    deallocateData(&data);
     free(result);
-    free(expectedResult);
+
+    MPI_Finalize();
 }
 
 int main(int argc, char ** argv) {
--- tests/various_s_parameter.c
+++ tests/various_s_parameter.c
@@ -2,30 +2,64 @@
 // Created by morfeush22 on 22.12.18.
 //
 
+#include "mpi.h"
 #include "test_base.h"
 #include "../src/base.h"
 #include "../src/chebyshev.h"
 
 void testDifferentSParameter() {
-    struct Data data = loadDataFromFile("sources/data");
-    int dimension = data.dimension;
+    int rank, size;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    FILE *fp;
+    int dimension;
+
+    if (rank == 0) {
+        fp = fopen("sources/data", "r");
+        if (fp == NULL) {
+            exit(EXIT_FAILURE);
+        }
+
+        dimension = numberOfLines(fp);
+    }
+
+    MPI_Bcast(&dimension, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    struct Data data = allocateData(dimension);
+    data.dimension = dimension;
+
+    if (rank == 0) {
+        parseData(fp, &data);
+        fclose(fp);
+    }
 
     int iterations = 0;
 
-    double *expectedResult = loadVectorFromFile("sources/data.expected_solution");
+    double * expectedResult = NULL;
+
+    if (rank == 0) {
+        expectedResult = loadVectorFromFile("sources/data.expected_solution");
+    }
 
     for (int sParameter = 1; sParameter < 100; ++sParameter) {
-        double *result = solveLinear(data, PRECISION, sParameter, &iterations);
+        double *result = solveLinear(data, PRECISION, sParameter, &iterations, rank, size);
 
-        for (int i = 0; i < dimension; ++i) {
-            ASSERT_EQUAL(expectedResult[i], result[i], PRECISION);
+        if (rank == 0) {
+            for (int i = 0; i < dimension; ++i) {
+                ASSERT_EQUAL(expectedResult[i], result[i], PRECISION);
+            }
         }
 
         free(result);
     }
 
-    deallocateData(data);
+    deallocateData(&data);
     free(expectedResult);
+
+    MPI_Finalize();
 }
 
 int main(int argc, char ** argv) {
